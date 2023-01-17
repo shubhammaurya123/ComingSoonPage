60cde10b9a3d457fd1fbe96c79601125d48195bf
Shubham Maurya <skmaurya9721@gmail.com>
2023-01-17 19:48:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { BsFacebook, BsTwitter, BsInstagram } from "react-icons/bs";
+import { AiFillLinkedin , AiOutlineCopyrightCircle } from "react-icons/ai";
 
+import pic from "./pic.jpg";
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={pic} alt="Snow" style={{ width: "100%", height: "100vh"  , opacity : "inherit"}} />
+      <div className="container_item">
+        <div className="Nav">
+          <span className="alive-text"> Alive. </span>
+          <button className="downlodebtn">Downlode</button>
+        </div>
+        <div className="center-text">
+          <div className="coming-text">Coming Soon...</div>
+          <div className="time-text">OUR WEBSITE IS COMING 2023</div>
+        </div>
+
+        <div className="bottom-split">
+          <div className="left-bottom">
+            <div className="left-bottom-item"> Launching in</div>
+            <div className="time">
+              <div className="day">
+                <h1> 23</h1>
+                <span> Day</span>
+              </div>
+              <div className="hour">
+                <h1> 04</h1>
+                <span>hour </span>
+              </div>
+              <div className="minute">
+                <h1> 55</h1>
+                <span> minut</span>
+              </div>
+              <div className="second">
+                <h1> 23</h1>
+                <span> second</span>
+              </div>
+            </div>
+          </div>
+          <div className="right-bottom">
+            <div className="right-item">
+              <div className="right-bottom-item">Follow Us :</div>
+              <BsFacebook />
+              <AiFillLinkedin />
+              <BsTwitter />
+              <BsInstagram />
+            </div>
+            <div className="copy-right-footer">
+              <AiOutlineCopyrightCircle/>
+              <span className="copy-right-text"> Shubham Maurya | All right reserved</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
